c04e09c006b2b96d455ff41ccff9f35715a25f21
Veljko Cerovic <68971458+xCera@users.noreply.github.com>
2020-09-01 19:01:54 +0200

    setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+	res.send('hello world');
+});
+
+app.listen(port, () => console.log(`listening!`));
