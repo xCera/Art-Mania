cbedf0d043593945a899db1b030035b2e0ad51c8
Veljko Cerovic <68971458+xCera@users.noreply.github.com>
2020-09-04 20:09:10 +0200

    code refactoring

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const artwork = require('./models/artwork');
-
 const express = require('express'),
 	app = express(),
 	port = 3000,
 	bodyParser = require('body-parser'),
 	mongoose = require('mongoose'),
-	methodOverride = require('method-override'),
-	Artwork = require('./models/artwork');
+	methodOverride = require('method-override');
+
+// REQUIRING ROUTES
+
+const artworksRoutes = require('./routes/artworks');
+
+// ===================== APPLICATIONS SETUP ============================
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
 mongoose.set('useFindAndModify', false);
+
 //======================== DATABASE SETUP =========================================
 mongoose
 	.connect('mongodb://localhost:27017/art_mania', {
@@ -24,68 +28,6 @@ mongoose
 
 //================================== ROUTES =========================================
 
-//	INDEX ROUTE - Show all artworks on page
-
-app.get('/artworks', (req, res) => {
-	Artwork.find({})
-		.then((allArtworks) => {
-			res.render('artworks', { artworks: allArtworks });
-		})
-		.catch((err) => console.log(err));
-});
-
-//	CREATE ROUTE - Get data from form then create new Artwork
-
-app.post('/artworks', (req, res) => {
-	Artwork.create({
-		title: req.body.title,
-		thumbnail: req.body.thumbnail,
-		desc: req.body.desc
-	})
-		.then((newArtwork) => {
-			console.log('New artwork succesfuly created');
-			console.log(newArtwork);
-		})
-		.catch((err) => console.log(err));
-	res.redirect('/artworks');
-});
-
-// NEW ROUTE - Show form to create new Artwork
-
-app.get('/artworks/new', (req, res) => {
-	res.render('newArtwork');
-});
-
-// SHOW ROUTE - Show info about one specific artwork
-
-app.get('/artworks/:id', (req, res) => {
-	let id = req.params.id;
-	Artwork.findById(id)
-		.then((artwork) => {
-			res.render('showArtwork', { artwork: artwork });
-		})
-		.catch((err) => console.log(err));
-});
-
-// EDIT ROUTE - Show form to edit artwork
-app.get('/artworks/:id/edit', (req, res) => {
-	let id = req.params.id;
-	Artwork.findById(id)
-		.then((artwork) => {
-			res.render('editArtwork', { artwork: artwork });
-		})
-		.catch((err) => console.log(err));
-});
-
-// UPDATE ROUTE - Get infro from form and update artowrk
-
-app.put('/artworks/:id', (req, res) => {
-	let id = req.params.id;
-	Artwork.findByIdAndUpdate(id, req.body, { new: true })
-		.then((updatedArtowrk) => {
-			res.redirect(`/artworks/${id}`);
-		})
-		.catch((err) => console.log(err));
-});
+app.use(artworksRoutes);
 
 app.listen(port, () => console.log(`listening!`));
---
+++ routes/artworks.js
@@ -0,0 +1,70 @@
+// ================== ARTWORKS ROUTE ======================
+const express = require('express'),
+	router = express.Router(),
+	Artwork = require('../models/artwork');
+
+//	INDEX ROUTE - Show all artworks on page
+
+router.get('/artworks', (req, res) => {
+	Artwork.find({})
+		.then((allArtworks) => {
+			res.render('artworks/artworks', { artworks: allArtworks });
+		})
+		.catch((err) => console.log(err));
+});
+
+//	CREATE ROUTE - Get data from form then create new Artwork
+
+router.post('/artworks', (req, res) => {
+	Artwork.create({
+		title: req.body.title,
+		thumbnail: req.body.thumbnail,
+		desc: req.body.desc
+	})
+		.then((newArtwork) => {
+			console.log('New artwork succesfuly created');
+			console.log(newArtwork);
+		})
+		.catch((err) => console.log(err));
+	res.redirect('/artworks');
+});
+
+// NEW ROUTE - Show form to create new Artwork
+
+router.get('/artworks/new', (req, res) => {
+	res.render('artworks/newArtwork');
+});
+
+// SHOW ROUTE - Show info about one specific artwork
+
+router.get('/artworks/:id', (req, res) => {
+	let id = req.params.id;
+	Artwork.findById(id)
+		.then((artwork) => {
+			res.render('artworks/showArtwork', { artwork: artwork });
+		})
+		.catch((err) => console.log(err));
+});
+
+// EDIT ROUTE - Show form to edit artwork
+router.get('/artworks/:id/edit', (req, res) => {
+	let id = req.params.id;
+	Artwork.findById(id)
+		.then((artwork) => {
+			res.render('artworks/editArtwork', { artwork: artwork });
+		})
+		.catch((err) => console.log(err));
+});
+
+// UPDATE ROUTE - Get infro from form and update artowrk
+
+router.put('/artworks/:id', (req, res) => {
+	let id = req.params.id;
+	Artwork.findByIdAndUpdate(id, req.body, { new: true })
+		.then((updatedArtowrk) => {
+			res.redirect(`/artworks/${id}`);
+		})
+		.catch((err) => console.log(err));
+});
+
+module.exports = router;
